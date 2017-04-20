54cfd926d7b25da039a9eeb1827f4bd7816a70f3
matamicen <matamicen@gmail.com>
2017-04-20 10:58:26 -0300

    Initial commit

---
+++ src/main/java/com/example/captton55/Demo55Application.java
@@ -0,0 +1,12 @@
+package com.example.captton55;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo55Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo55Application.class, args);
+	}
+}
